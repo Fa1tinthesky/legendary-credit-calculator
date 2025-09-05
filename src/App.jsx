8d59ac0cc6284fa100d6fb3ea7368d1e2290f2ce
Muhammad <kayumovmuhamma@gmail.com>
2025-09-05 20:11:34 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TableComponent from "./components/TableComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <Router>
-        <Routes>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='*' element={<h1>404. Not found</h1>}></Route> 
-        </Routes>
-      </Router>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <TableComponent
+                            table={[
+                                {
+                                    date: "Март, 2025",
+                                    sum: 14977.23,
+                                    main_debt: 10477.23,
+                                    percent: 4500.0,
+                                    remain: 289522.77,
+                                },
+                                {
+                                    date: "Март, 2025",
+                                    sum: 14977.23,
+                                    main_debt: 10477.23,
+                                    percent: 4500.0,
+                                    remain: 289522.77,
+                                },
+                                {
+                                    date: "Март, 2025",
+                                    sum: 14977.23,
+                                    main_debt: 10477.23,
+                                    percent: 4500.0,
+                                    remain: 289522.77,
+                                },
+                                {
+                                    date: "Март, 2025",
+                                    sum: 14977.23,
+                                    main_debt: 10477.23,
+                                    percent: 4500.0,
+                                    remain: 289522.77,
+                                },
+                            ]}
+                        />
+                    }
+                ></Route>
+                <Route path="*" element={<h1>404. Not found</h1>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;

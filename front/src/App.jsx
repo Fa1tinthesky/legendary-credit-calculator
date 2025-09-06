efd2e81156c6ad205478498cad148f973255f23c
Muhammad <kayumovmuhamma@gmail.com>
2025-09-06 16:32:40 +0500

    v 0.4

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,28 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContentComponent from "./components/ContentComponent/ContentComponent";
 import { useEffect, useState } from "react";
-
-function getData(setData, setIsLoading, body) {
-    setIsLoading(true);
-    fetch("http://10.192.9.134:8080/api/calculate", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-    })
-        .then((resp) => {
-            return resp.json();
-        })
-        .then((response) => {
-            let newResponse = {
-                ...response,
-                table: response.table.map((item) => {
-                    return { ...item, date: item.date.slice(0, 10) };
-                }),
-            };
-            setData(newResponse);
-            setIsLoading(false);
-        });
-}
+import getData from "./functions/getData";
 
 function App() {
     const [isLoading, setIsLoading] = useState(true);
@@ -38,8 +17,6 @@ function App() {
             start_date: "2025-09-03",
         });
     }, []);
-
-    console.log(data);
 
     return (
         <Router>
